cfa2488e920a606c51182ed6e0f74535c0e2947c
Kim Fransson <kim.fransson.22@gmail.com>
2025-04-09 21:38:40 +0200

    updated text

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -6,16 +6,18 @@ export const Introduction = () => {
     <div className={twJoin(["flex flex-col items-center", "md:flex-row"])}>
       <ProfileImage className="md:order-2 md:-mr-4" />
       <div>
-        <h2 className="text-4xl font-bold text-balance text-center mt-8 md:text-left md:text-7xl lg:text-8xl">
-          Nice to meet you! I&rsquo;m{" "}
-          <span className="underline decoration-4 underline-offset-6 decoration-primary">
+        <h2 className="text-4xl font-bold text-balance text-center mt-8 md:text-left md:text-6xl lg:text-7xl">
+          Thanks for stopping by! I&rsquo;m{" "}
+          <strong className="underline decoration-4 underline-offset-6 decoration-primary">
             Kim Fransson
-          </span>
+          </strong>
           .
         </h2>
-        <p className="text-center mt-6 md:text-left md:text-lg md:mt-12 lg:max-w-[445px]">
-          Based in the Sweden, I&rsquo;m a front-end developer passionate about
-          building accessible web apps that users love.
+        <p className="text-center text-balance mt-6 md:text-left md:text-lg md:mt-12 lg:max-w-[445px]">
+          Detail-oriented frontend engineer with +6 years of experience,
+          including building applications from the backend side, bringing
+          expertise in both frontend and backend development to create robust,
+          end-to-end solutions.
         </p>
       </div>
     </div>
