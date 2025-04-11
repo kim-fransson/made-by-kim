521c7593b9ed4baadc0b65bd9c5ed5383d4b01c3
Kim Fransson <kim.fransson.22@gmail.com>
2025-04-11 19:57:30 +0200

    added focus-visible for cta link

--- src/components/CTALink.tsx
+++ src/components/CTALink.tsx
@@ -2,23 +2,26 @@
 
 import { motion } from "motion/react";
 import { Link, LinkProps } from "./ui";
-import { useHover } from "react-aria";
+import { mergeProps, useFocusRing, useHover } from "react-aria";
+import { twJoin } from "tailwind-merge";
 
 export const CTALink = ({ children, ...props }: LinkProps) => {
   const { isHovered, hoverProps } = useHover({});
-  // const { isFocusVisible, focusProps } = useFocusRing();
+  const { isFocusVisible, focusProps } = useFocusRing();
   return (
     <div {...hoverProps} className="relative">
       <motion.div
         className="absolute top-1/2 -translate-y-1/2 size-12 bg-white rounded-full origin-left pointer-coarse:hidden block"
         initial={{ width: "48px" }}
-        animate={{ width: isHovered ? "100%" : "48px" }}
+        animate={{ width: isHovered || isFocusVisible ? "100%" : "48px" }}
         transition={{ type: "spring", stiffness: 200, damping: 20 }}
       ></motion.div>
       <Link
-        {...props}
-        // {...focusProps}
-        className="text-center pointer-coarse:bg-primary pointer-coarse:text-foreground-primary uppercase font-bold tracking-widest py-3 px-5 relative mix-blend-difference flex items-center gap-2"
+        {...mergeProps(hoverProps, focusProps, props)}
+        className={twJoin(
+          "text-center uppercase font-bold tracking-widest py-3 px-5 relative flex items-center gap-2 outline-offset-4",
+          "pointer-coarse:bg-primary pointer-coarse:text-foreground-primary mix-blend-difference"
+        )}
       >
         {children}
       </Link>
