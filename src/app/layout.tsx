27f44897a88a3e47ee9da60abf0020f4c634534d
Kim Fransson <kim.fransson.22@gmail.com>
2025-04-11 19:22:27 +0200

    smooth scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`font-sans min-h-svh bg-background-100 text-foreground ${spaceGrotesk.variable} antialiased`}
       >
